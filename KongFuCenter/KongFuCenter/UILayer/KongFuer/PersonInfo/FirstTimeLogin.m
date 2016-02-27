0d719bc8f38d23bb0256971da14c106ca7c2fb2e
王建成 <w754550398@qq.com>
2016-02-27 08:46:06 +0800

    添加没有设置用户名的，必须设置用户名功能

---
+++ KongFuCenter/KongFuCenter/UILayer/KongFuer/PersonInfo/FirstTimeLogin.m
@@ -0,0 +1,13 @@
+//
+//  FirstTimeLogin.m
+//  KongFuCenter
+//
+//  Created by Wangjc on 16/2/26.
+//  Copyright © 2016年 zykj. All rights reserved.
+//
+
+#import "FirstTimeLogin.h"
+
+@implementation FirstTimeLogin
+
+@end
